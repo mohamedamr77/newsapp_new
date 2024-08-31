13b47b17bdb9392c9bae67a4c0ad884f816f4f63
mohamedamr77 <moamr947@gmail.com>
2024-08-31 22:29:50 +0300

    add bottomNavigationBar

--- lib/features/home_page/presentation/view/view.dart
+++ lib/features/home_page/presentation/view/view.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:newsappcode/features/home_page/presentation/view/widgets/home_page_body.dart';
 
-class HomePageScreen extends StatelessWidget {
+import '../../../../core/utils/color_app.dart';
+
+class HomePageScreen extends StatefulWidget {
   const HomePageScreen({super.key});
 
   @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  int currentIndex=0;
+  List<Widget> displayScreen=[
+    const HomePageBody(),
+    const Text("Mohamed amr"),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       backgroundColor: Colors.white,
-      body: HomePageBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index){
+          setState(() {
+            currentIndex=index;
+          });
+        },
+        currentIndex: currentIndex,
+        selectedItemColor:Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.abc),
+              label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ac_unit),
+            label: "abo Amr",
+          ),
+        ],
+      ),
+      body: displayScreen[currentIndex],
     );
   }
 }
