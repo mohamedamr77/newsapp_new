da8dffc392acdd37d9962dad9e03e71eb4823002
mohamedamr77 <moamr947@gmail.com>
2024-09-25 23:04:13 +0300

    finish design item general news home

--- lib/features/home_page/presentation/view/widgets/list_view_body.dart
+++ lib/features/home_page/presentation/view/widgets/list_view_body.dart
@@ -4,6 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:newsappcode/features/home_page/data/model/home_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 class ListViewBody extends StatelessWidget {
   const ListViewBody({super.key, required this.articlesModel});
 
@@ -11,6 +13,8 @@ class ListViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DateTime articleTime = DateTime.parse(articlesModel.publishedAt!); // Parse the string to DateTime
+    String timeAgo = timeago.format(articleTime, locale: 'en_ short'); // Convert to time ago format
     return Padding(
       padding: const EdgeInsets.only(bottom: 20, left: 24, right: 24),
       child: Column(
@@ -44,18 +48,7 @@ class ListViewBody extends StatelessWidget {
               errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 12),
-            child: Text(
-              "GAZA",
-              textAlign: TextAlign.start,
-              style: GoogleFonts.poppins(
-                fontSize: 13,
-                fontWeight: FontWeight.w400,
-                color: const Color(0xff4E4B66),
-              ),
-            ),
-          ),
+          SizedBox(height: 12.h,),
           Padding(
             padding: const EdgeInsets.only(left: 12),
             child: Text(
@@ -73,19 +66,19 @@ class ListViewBody extends StatelessWidget {
             padding: const EdgeInsets.only(left: 12, top: 6),
             child: Row(
               children: [
-                const CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 10,
-                  child: Image(
-                    image: AssetImage("assets/images/bbc news.png"),
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
+                 CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 8.w,
+                  // child: Image(
+                  //   image: AssetImage("assets/images/bbc news.png"),
+                  //   fit: BoxFit.cover,
+                  //   width: double.infinity,
+                  //   height: double.infinity,
+                  // ),
                 ),
                 const SizedBox(width: 6),
                 Text(
-                  'BBC News',
+                  articlesModel.source!.name!,
                   style: GoogleFonts.poppins(
                     fontSize: 13.sp,
                     fontWeight: FontWeight.w600,
@@ -104,15 +97,16 @@ class ListViewBody extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(width: 6),
-                Text(
-                  '7h ago',
-                  style: GoogleFonts.poppins(
-                    fontSize: 13.sp,
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xff4E4B66),
+                Expanded(
+                  child: Text(
+                    timeAgo,
+                    style: GoogleFonts.poppins(
+                      fontSize: 13.sp,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xff4E4B66),
+                    ),
                   ),
                 ),
-                const Spacer(),
                 GestureDetector(
                   onTap: () {},
                   child: const Icon(Icons.bookmark_border),
