8ce4aa0929d7f7ce60e793e0102f1621e86f6f97
mohamedamr77 <moamr947@gmail.com>
2024-10-17 03:53:11 +0300

    make url launcher

---
+++ lib/core/utils/launch_url.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+Future<void> launchCustomUrl(
+    {required String url, required BuildContext context}) async {
+  final Uri parsedUrl = Uri.parse(url);
+  if (await canLaunchUrl(parsedUrl)) {
+    !await launchUrl(parsedUrl);
+  } else {
+
+    Fluttertoast.showToast(
+      msg: "The Url Is Not Valied",
+      fontSize: 16,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      timeInSecForIosWeb: 2,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      webShowClose: true,
+    );
+  }
+}
--- lib/features/home_page/presentation/view/widgets/list_view_body.dart
+++ lib/features/home_page/presentation/view/widgets/list_view_body.dart
@@ -6,6 +6,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
+import '../../../../../core/utils/launch_url.dart';
+
 class ListViewBody extends StatelessWidget {
   const ListViewBody({super.key, required this.articlesModel});
 
@@ -19,107 +21,112 @@ class ListViewBody extends StatelessWidget {
         locale: 'en_ short'); // Convert to time ago format
     return Padding(
       padding: const EdgeInsets.only(bottom: 20, left: 24, right: 24),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16.w),
-            child: CachedNetworkImage(
-              fit: BoxFit.fill,
-              height: 195.h,
-              width: double.infinity,
-              imageUrl: articlesModel.urlToImage ?? "",
-              progressIndicatorBuilder: (context, url, downloadProgress) =>
-                  SizedBox(
+      child: GestureDetector(
+        onTap: (){
+          launchCustomUrl(url: articlesModel.url??"", context: context);
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16.w),
+              child: CachedNetworkImage(
+                fit: BoxFit.fill,
                 height: 195.h,
                 width: double.infinity,
-                child: Shimmer.fromColors(
-                  baseColor: Colors.grey[600]!,
-                  highlightColor: Colors.grey[400]!,
-                  direction: ShimmerDirection.ltr, // Left to right shimmer
-                  child: Container(
-                    height: 195.h,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[600],
-                      borderRadius: BorderRadius.circular(12),
+                imageUrl: articlesModel.urlToImage ?? "",
+                progressIndicatorBuilder: (context, url, downloadProgress) =>
+                    SizedBox(
+                  height: 195.h,
+                  width: double.infinity,
+                  child: Shimmer.fromColors(
+                    baseColor: Colors.grey[600]!,
+                    highlightColor: Colors.grey[400]!,
+                    direction: ShimmerDirection.ltr, // Left to right shimmer
+                    child: Container(
+                      height: 195.h,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            ),
-          ),
-          SizedBox(
-            height: 12.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 12),
-            child: Text(
-              articlesModel.title ?? "",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: GoogleFonts.poppins(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: Colors.black87,
+                errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 12, top: 6),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  radius: 8.w,
-                  // child: Image(
-                  //   image: AssetImage("assets/images/bbc news.png"),
-                  //   fit: BoxFit.cover,
-                  //   width: double.infinity,
-                  //   height: double.infinity,
-                  // ),
+            SizedBox(
+              height: 12.h,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Text(
+                articlesModel.title ?? "",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black87,
                 ),
-                const SizedBox(width: 6),
-                Text(
-                  articlesModel.source!.name!,
-                  style: GoogleFonts.poppins(
-                    fontSize: 13.sp,
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xff4E4B66),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 6),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.blue,
+                    radius: 8.w,
+                    // child: Image(
+                    //   image: AssetImage("assets/images/bbc news.png"),
+                    //   fit: BoxFit.cover,
+                    //   width: double.infinity,
+                    //   height: double.infinity,
+                    // ),
                   ),
-                ),
-                const SizedBox(width: 12),
-                const CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 7,
-                  child: Image(
-                    image: AssetImage("assets/images/time.png"),
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                ),
-                const SizedBox(width: 6),
-                Expanded(
-                  child: Text(
-                    timeAgo,
+                  const SizedBox(width: 6),
+                  Text(
+                    articlesModel.source!.name!,
                     style: GoogleFonts.poppins(
                       fontSize: 13.sp,
                       fontWeight: FontWeight.w600,
                       color: const Color(0xff4E4B66),
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Icon(Icons.bookmark_border),
-                ),
-              ],
+                  const SizedBox(width: 12),
+                  const CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 7,
+                    child: Image(
+                      image: AssetImage("assets/images/time.png"),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                  const SizedBox(width: 6),
+                  Expanded(
+                    child: Text(
+                      timeAgo,
+                      style: GoogleFonts.poppins(
+                        fontSize: 13.sp,
+                        fontWeight: FontWeight.w600,
+                        color: const Color(0xff4E4B66),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Icon(Icons.bookmark_border),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
