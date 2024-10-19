626d04da85fcc931e92f2651c461474f702265bd
mohamedamr77 <moamr947@gmail.com>
2024-10-19 14:59:16 +0300

    32

--- lib/features/home_page/data/repo/home_implement.dart
+++ lib/features/home_page/data/repo/home_implement.dart
@@ -15,7 +15,7 @@ class HomeImplement implements HomeRepo {
     try {
       var response = await apiService.get(
           endPoint:
-              "/everything?q=gaza&apiKey=08433c8dda43458fa30826408cb8219e");
+              "/top-headlines?category=general&apiKey=08433c8dda43458fa30826408cb8219e");
       List<dynamic> articlesFromApi = response["articles"];
       List<ArticlesModel> articlesList = [];
       for (int i = 0; i < articlesFromApi.length; i++) {
