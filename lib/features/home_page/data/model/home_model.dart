7e02a4cde65109c7daefd2cd5c24f87aaf019264
mohamedamr77 <moamr947@gmail.com>
2024-10-19 23:33:20 +0300

    make model equatble

--- lib/features/home_page/data/model/home_model.dart
+++ lib/features/home_page/data/model/home_model.dart
@@ -1,94 +1,106 @@
-class NewsModel {
-  String? status;
-  int? totalResults;
-  List<ArticlesModel>? articles;
+import 'package:equatable/equatable.dart';
+
+class NewsModel extends Equatable {
+  final String? status;
+  final int? totalResults;
+  final List<ArticlesModel>? articles;
 
   NewsModel({this.status, this.totalResults, this.articles});
 
-  NewsModel.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    totalResults = json['totalResults'];
-    if (json['articles'] != null) {
-      articles = <ArticlesModel>[];
-      json['articles'].forEach((v) {
-        articles!.add(ArticlesModel.fromJson(v));
-      });
-    }
-  }
+  NewsModel.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        totalResults = json['totalResults'],
+        articles = json['articles'] != null
+            ? List<ArticlesModel>.from(
+            json['articles'].map((v) => ArticlesModel.fromJson(v)))
+            : null;
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['status'] = status;
-    data['totalResults'] = totalResults;
-    if (articles != null) {
-      data['articles'] = articles!.map((v) => v.toJson()).toList();
-    }
-    return data;
+    return {
+      'status': status,
+      'totalResults': totalResults,
+      'articles': articles?.map((v) => v.toJson()).toList(),
+    };
   }
+
+  @override
+  List<Object?> get props => [status, totalResults, articles];
 }
 
-class ArticlesModel {
-  Source? source;
-  String? author;
-  String? title;
-  String? description;
-  String? url;
-  String? urlToImage;
-  String? publishedAt;
-  String? content;
+class ArticlesModel extends Equatable {
+  final Source? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
 
-  ArticlesModel(
-      {this.source,
-      this.author,
-      this.title,
-      this.description,
-      this.url,
-      this.urlToImage,
-      this.publishedAt,
-      this.content});
+  ArticlesModel({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
 
-  ArticlesModel.fromJson(Map<String, dynamic> json) {
-    source = json['source'] != null ? Source.fromJson(json['source']) : null;
-    author = json['author'];
-    title = json['title'];
-    description = json['description'];
-    url = json['url'];
-    urlToImage = json['urlToImage'];
-    publishedAt = json['publishedAt'];
-    content = json['content'];
-  }
+  ArticlesModel.fromJson(Map<String, dynamic> json)
+      : source = json['source'] != null ? Source.fromJson(json['source']) : null,
+        author = json['author'],
+        title = json['title'],
+        description = json['description'],
+        url = json['url'],
+        urlToImage = json['urlToImage'],
+        publishedAt = json['publishedAt'],
+        content = json['content'];
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    if (source != null) {
-      data['source'] = source!.toJson();
-    }
-    data['author'] = author;
-    data['title'] = title;
-    data['description'] = description;
-    data['url'] = url;
-    data['urlToImage'] = urlToImage;
-    data['publishedAt'] = publishedAt;
-    data['content'] = content;
-    return data;
+    return {
+      'source': source?.toJson(),
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt,
+      'content': content,
+    };
   }
+
+  @override
+  List<Object?> get props => [
+    source,
+    author,
+    title,
+    description,
+    url,
+    urlToImage,
+    publishedAt,
+    content,
+  ];
 }
 
-class Source {
-  String? id;
-  String? name;
+class Source extends Equatable {
+  final String? id;
+  final String? name;
 
   Source({this.id, this.name});
 
-  Source.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-  }
+  Source.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'];
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
-    return data;
+    return {
+      'id': id,
+      'name': name,
+    };
   }
+
+  @override
+  List<Object?> get props => [id, name];
 }
